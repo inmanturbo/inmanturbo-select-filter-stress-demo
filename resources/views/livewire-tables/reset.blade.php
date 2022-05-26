13c700a116257cb1ea808a0e32dfcf33d2d7fe1b
= <inman.turbo@gmail.com>
2022-05-26 17:52:11 -0400

    work in progress

--- resources/views/livewire-tables/reset.blade.php
+++ resources/views/livewire-tables/reset.blade.php
@@ -1,3 +1,3 @@
-<button wire:click="resetTable" class="block items-center justify-center transition duration-150 ease-in-out text-[#0000FF] border-gray-300 rounded-md shadow-sm sm:text-sm sm:leading-5 dark:bg-gray-700 dark:text-white dark:border-gray-600">
+<button wire:click="resetTable" class="inline-flex justify-center items-center p-1 text-sm text-[#0000FF] bg-white border border-gray-300 rounded-md shadow-sm hover:bg-gray-50 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 dark:bg-gray-700 dark:text-white dark:border-gray-600 dark:hover:bg-gray-600">
   <span>{{ __('Reset') }}</span>
 </button>
--- resources/views/livewire-tables/year-to-date.blade.php
+++ resources/views/livewire-tables/year-to-date.blade.php
@@ -1,5 +1,5 @@
 <div>
-    <button wire:click="$emit('setFilter', 'date_from', '{{ now()->startOfYear()->format('Y-m-d') }}')" class="block w-full transition duration-150 ease-in-out border-gray-300 rounded-md shadow-sm sm:text-sm sm:leading-5 dark:bg-gray-700 dark:text-white dark:border-gray-600">
+    <button wire:click="$emit('setFilter', 'date_from', '{{ now()->startOfYear()->format('Y-m-d') }}')" class="inline-flex justify-center items-center py-2 px-1 text-sm text-[#0000FF] bg-white border border-gray-300 rounded-md shadow-sm hover:bg-gray-50 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 dark:bg-gray-700 dark:text-white dark:border-gray-600 dark:hover:bg-gray-600">
         {{ __('Year to Date') }}
     </button>
 </div>
